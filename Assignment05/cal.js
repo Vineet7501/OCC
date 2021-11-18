1d4732b0de80cd6cbc3f886ac2d767fc1df1556e
Vineet7501 <66490255+Vineet7501@users.noreply.github.com>
2021-11-18 12:03:18 +0530

    Add files via upload

---
+++ Assignment05/cal.js
@@ -0,0 +1,16 @@
+// Create Calculator using Clousure
+const cal = (function () {
+    let num = 10;
+    return function () {
+      add = num += 1;
+      sub = num -= 2;
+      mul = num *= 1;
+      div = num /= 2;
+      console.log("addition: " + add);
+      console.log("Subtraction: " + sub);
+      console.log("Multiplication: " + mul);
+      console.log("Division: " + div);
+    };
+  })();
+  
+  cal();
