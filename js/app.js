c0919ffcadfbae897da14895fb405345845f19a1
Vineet7501 <66490255+Vineet7501@users.noreply.github.com>
2021-12-22 14:50:20 +0530

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const viewModel = {
-    gender: ko.observable("male"),
-  };
-  viewModel.applyGender = ko.pureComputed(function () {
-    return this.gender() == "male" ? "male" : "female";
-  }, viewModel);
-  
-  ko.applyBindings(viewModel);
